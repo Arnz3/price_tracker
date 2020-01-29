028a66d3147a0b50f17abefc3b941a0a6b247188
Arnz3 <cuyversarno@gmail.com>
2020-01-29 21:36:51 +0100

    changes

--- price_tracker.py
+++ price_tracker.py
@@ -1,13 +1,14 @@
-import webbrowser
-import bs4
 import requests
+from bs4 import BeautifulSoup
 
+URL = "https://www.amazon.com/dp/B07SXMZLPK?tag=pcpapi-20&linkCode=ogi&th=1&psc=1"
 
-def track_price(part, URL):
-    res = requests.get(URL)
-    if res.raise_for_status == none:
-        soup = bs4.BeautifulSoup(res.text, "html.parser")
-        elems = soup.select(//*[@id="priceblock_ourprice"])
-        price = elems[0].text.strip()
-    else:
-        return fout
+headers = {
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
+
+page = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+title = soup.find(id="productTitle")
+print(title)
